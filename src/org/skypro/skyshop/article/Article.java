02f09d22e169b8af7f9d556885f956a540b5a53b
Vladimirs Anufrijevs <vladimirs.anufrijevs@balticom.eu>
2025-03-01 14:17:22 +0200

    add hw4

---
+++ src/org/skypro/skyshop/article/Article.java
@@ -0,0 +1,41 @@
+package org.skypro.skyshop.article;
+
+import org.skypro.skyshop.search.Searchable;
+
+public class Article implements Searchable {
+    private final String title;
+    private final String content;
+
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    @Override
+    public String toString() {
+        return title + "\n" + content;
+    }
+
+    @Override
+    public String getSearchTerm() {
+        return toString();
+    }
+
+    @Override
+    public String getContentType() {
+        return "ARTICLE";
+    }
+
+    @Override
+    public String getName() {
+        return title;
+    }
+}
